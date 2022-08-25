adbf62712cca68512c401aab7758c9aa3164f8a5
mingcan <1085914019@qq.com>
2022-08-25 11:56:56 +0800

    init

--- vue3-test/src/service/user.js
+++
@@ -1,8 +0,0 @@
-import axios from "../utils/axios";
-export function login(params){
-    return axios.post('user/login',params)
-}
-
-export function register(params){
-    return axios.post("/user/register",params)
-}
